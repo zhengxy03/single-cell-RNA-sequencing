51366c632100b38b27c0a5ef175ef20f6953435e
zhengxy03 <1730861545@qq.com>
2025-01-10 15:29:52 +0800

    modified

--- ESCC/single-cell transcriptome analysis/data_import.r
+++ ESCC/single-cell transcriptome analysis/data_import.r
@@ -1,10 +1,22 @@
 library(Seurat)
+library(ggplot2)
 #format 1:barcodes.tsv.gz(cell barcode),features.tsv.gz(gene ID),matrix.mtx.gz
 #GSE203115
-setwd("E:/project/ESCC/GSE203115/")
-counts_matrix1 <- Read10X(".") #integrate three files
-class(counts_matrix1) #get expressin matrix, one row for a gene and one column for a cell (in sparse matrix dgCMatrix format)
+setwd("E:/project/ESCC/GSE203115/matrix")
+dir1 = "E:/project/ESCC/GSE203115/matrix/Esc1"
+dir2 = "E:/project/ESCC/GSE203115/matrix/Esc2"
+dir3 = "E:/project/ESCC/GSE203115/matrix/Esc3"
+
+counts_matrix1 <- Read10X(data.dir = dir1) #renamed and integrate three files
+counts_matrix2 <- Read10X(data.dir = dir2)
+counts_matrix3 <- Read10X(data.dir = dir3)
+
+class(counts_matrix1) 
+#get expressin matrix, one row for a gene and one column for a cell (in sparse matrix dgCMatrix format)
+
 seurat_obj1 <- CreateSeuratObject(counts = counts_matrix1) #create Seurat object
+seurat_obj2 <- CreateSeuratObject(counts = counts_matrix2)
+seurat_obj3 <- CreateSeuratObject(counts = counts_matrix3)
 
 #format 2:counts_matrix.csv.gz
 setwd()
