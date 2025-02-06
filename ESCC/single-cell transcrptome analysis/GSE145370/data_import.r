e49678a5982bfa7a739e26ebc6d5307a8ed1f000
zhengxy03 <1730861545@qq.com>
2025-02-06 20:56:24 +0800

    new file

---
+++ ESCC/single-cell transcrptome analysis/GSE145370/data_import.r
@@ -0,0 +1,29 @@
+library(Seurat)
+library(ggplot2)
+library(dplyr)
+
+base_dir <- "E:/project/ESCC/GSE145370/matrix/"
+dirs <- list.dirs(base_dir, full.names = TRUE, recursive = FALSE)
+
+sample_types <- ifelse(1:length(dirs) %% 2 == 1, "Tumor", "Normal")
+sample_sources <- c("S133", "S133", "S134", "S134", "S135", "S135", "S149", "S149", "S150", "S150", "S158", "S158", "S159", "S159")
+
+seurat_objs <- list()
+for (i in seq_along(dirs)) {
+  dir <- dirs[i]
+  print(paste("current_dir:", dir))
+  counts_matrix <- Read10X(data.dir = dir)
+  seurat_obj <- CreateSeuratObject(counts = counts_matrix, project = paste0("Sample", i))
+
+  seurat_obj <- AddMetaData(seurat_obj, metadata = data.frame(
+    sample_type = sample_types[i],
+    sample_sources = sample_sources[i]
+  ))
+
+  seurat_objs[[i]] <- seurat_obj
+}
+print(seurat_objs)
+
+merged_seurat_obj <- merge(seurat_objs[[1]], y = seurat_objs[-1], add.cell.ids = paste0("Sample", 1:length(dirs)))
+merged_seurat_obj <- JoinLayers(merged_seurat_obj)
+
