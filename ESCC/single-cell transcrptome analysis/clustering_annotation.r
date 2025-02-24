c25f25355b54af7be6e4e7a9b0cd882137bd9074
zhengxy03 <1730861545@qq.com>
2025-02-24 10:27:12 +0800

    modified

--- ESCC/single-cell transcrptome analysis/clustering_annotation.r
+++ ESCC/single-cell transcrptome analysis/clustering_annotation.r
@@ -21,36 +21,52 @@ merged_seurat_obj <- RunUMAP(merged_seurat_obj, reduction = "harmony", dims = 1:
 
 npg_pal <- pal_npg()(10)
 npg_extended <- colorRampPalette(npg_pal)(23)
-png("clusters.png", width = 3000, height = 3000, res = 300)  # 设置高分辨率和尺寸
+
+# 获取图例的个数和名称长度
+seurat_clusters <- as.character(unique(merged_seurat_obj@meta.data$seurat_clusters))  # 转换为字符向量
+num_legend_items <- length(seurat_clusters)  # 图例的个数
+max_label_length <- max(nchar(seurat_clusters))  # 图例名称的最大长度
+
+# 动态计算图片尺寸
+base_width <- 3000  # 基础宽度
+base_height <- 3000  # 基础高度
+legend_width_factor <- 100  # 每个图例项增加的宽度
+label_length_factor <- 10  # 每个字符增加的宽度
+
+# 计算动态宽度
+dynamic_width <- base_width + (num_legend_items * legend_width_factor) + (max_label_length * label_length_factor)
+
+# 导出图片
+png("clusters.png", width = dynamic_width, height = base_height, res = 300)
 DimPlot(merged_seurat_obj, reduction = "umap", label = TRUE, pt.size = 1, label.size = 8) +
-  xlab("UMAP_1") +
-  ylab("UMAP_2") +
-  ggtitle(NULL) +
-  scale_color_manual(values = npg_extended) +
-  coord_fixed(ratio = 1) +
-  guides(color = guide_legend(title = NULL, override.aes = list(size = 5))) +
-  theme(
-    text = element_text(size = 12, face = "bold"),
-    axis.text.x = element_text(size = 28, color = "black"),
-    axis.text.y = element_text(size = 28, color = "black"),
-    axis.title.x = element_text(size = 36, face = "bold", color = "black"),
-    axis.title.y = element_text(size = 36, face = "bold", color = "black"),
-    plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
-    legend.text = element_text(size = 28, face = "bold", color = "black"),
-    legend.title = element_text(size = 28, face = "bold", color = "black"),
-    legend.position = "right",
-    legend.box.margin = margin(0, 0, 0, 0),
-    legend.key = element_blank(),
-    legend.background = element_blank(),
-    panel.background = element_rect(fill = "white", color = NA),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.border = element_blank(),
-    axis.line.x = element_line(color = "black", linewidth = 0.5),
-    axis.line.y = element_line(color = "black", linewidth = 0.5),
-    aspect.ratio = 1,
-    plot.margin = margin(10, 50, 10, 10)
-  )
+    xlab("UMAP_1") +
+    ylab("UMAP_2") +
+    ggtitle(NULL) +
+    scale_color_manual(values = npg_extended) +
+    coord_fixed(ratio = 1) +
+    guides(color = guide_legend(title = NULL, override.aes = list(size = 5))) +
+    theme(
+        text = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 28, color = "black"),
+        axis.text.y = element_text(size = 28, color = "black"),
+        axis.title.x = element_text(size = 36, face = "bold", color = "black"),
+        axis.title.y = element_text(size = 36, face = "bold", color = "black", margin = margin(r = 20)),  # 增加右侧间距
+        plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        legend.text = element_text(size = 28, face = "bold", color = "black"),
+        legend.title = element_text(size = 28, face = "bold", color = "black"),
+        legend.position = "right",
+        legend.box.margin = margin(0, 0, 0, 0),
+        legend.key = element_blank(),
+        legend.background = element_blank(),
+        panel.background = element_rect(fill = "white", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line.x = element_line(color = "black", linewidth = 0.5),
+        axis.line.y = element_line(color = "black", linewidth = 0.5),
+        aspect.ratio = 1,
+        plot.margin = margin(10, 50, 10, 10)
+    )
 dev.off()
 
 #plot on samples
@@ -139,36 +155,52 @@ cell_type <- identity_mapping[merged_seurat_obj@meta.data$seurat_clusters]
 merged_seurat_obj@meta.data$cell_type <- cell_type
 
 npg_extended <- colorRampPalette(npg_pal)(23)
-png("annotation.png", width = 3000, height = 3000, res = 300)  # 设置高分辨率和尺寸
-DimPlot(merged_seurat_obj, reduction = "umap", label = TRUE, pt.size = 1, group.by = "cell_type") +
-  xlab("UMAP_1") +
-  ylab("UMAP_2") +
-  ggtitle(NULL) +
-  scale_color_manual(values = npg_extended) +
-  coord_fixed(ratio = 1) +
-  guides(color = guide_legend(title = NULL, override.aes = list(size = 5))) +
-  theme(
-    text = element_text(size = 12, face = "bold"),
-    axis.text.x = element_text(size = 12, color = "black"),
-    axis.text.y = element_text(size = 12, color = "black"),
-    axis.title.x = element_text(size = 14, face = "bold", color = "black"),
-    axis.title.y = element_text(size = 14, face = "bold", color = "black"),
-    plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
-    legend.text = element_text(size = 10, face = "bold", color = "black"),
-    legend.title = element_text(size = 10, face = "bold", color = "black"),
-    legend.position = "right",
-    legend.box.margin = margin(0, 0, 0, 0),
-    legend.key = element_blank(),
-    legend.background = element_blank(),
-    panel.background = element_rect(fill = "white", color = NA),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.border = element_blank(),
-    axis.line.x = element_line(color = "black", linewidth = 0.5),
-    axis.line.y = element_line(color = "black", linewidth = 0.5),
-    aspect.ratio = 1,
-    plot.margin = margin(10, 50, 10, 10)
-  )
+
+# 获取图例的个数和名称长度
+cell_types <- unique(merged_seurat_obj@meta.data$cell_type)
+num_legend_items <- length(cell_types)  # 图例的个数
+max_label_length <- max(nchar(cell_types))  # 图例名称的最大长度
+
+# 动态计算图片尺寸
+base_width <- 3000  # 基础宽度
+base_height <- 3000  # 基础高度
+legend_width_factor <- 100  # 每个图例项增加的宽度
+label_length_factor <- 10  # 每个字符增加的宽度
+
+# 计算动态宽度
+dynamic_width <- base_width + (num_legend_items * legend_width_factor) + (max_label_length * label_length_factor)
+
+# 导出图片
+png("annotation.png", width = dynamic_width, height = base_height, res = 300)
+DimPlot(merged_seurat_obj, reduction = "umap", label = TRUE, pt.size = 1, group.by = "cell_type", label.size = 8) +
+    xlab("UMAP_1") +
+    ylab("UMAP_2") +
+    ggtitle(NULL) +
+    scale_color_manual(values = npg_extended) +
+    coord_fixed(ratio = 1) +
+    guides(color = guide_legend(title = NULL, override.aes = list(size = 5))) +
+    theme(
+        text = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 28, color = "black"),
+        axis.text.y = element_text(size = 28, color = "black"),
+        axis.title.x = element_text(size = 36, face = "bold", color = "black"),
+        axis.title.y = element_text(size = 36, face = "bold", color = "black", margin = margin(r = 20)),  # 增加右侧间距
+        plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        legend.text = element_text(size = 28, face = "bold", color = "black"),
+        legend.title = element_text(size = 28, face = "bold", color = "black"),
+        legend.position = "right",
+        legend.box.margin = margin(0, 0, 0, 0),
+        legend.key = element_blank(),
+        legend.background = element_blank(),
+        panel.background = element_rect(fill = "white", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line.x = element_line(color = "black", linewidth = 0.5),
+        axis.line.y = element_line(color = "black", linewidth = 0.5),
+        aspect.ratio = 1,
+        plot.margin = margin(10, 50, 10, 10)
+    )
 dev.off()
 
 #plot according to sample types
@@ -251,10 +283,12 @@ ggplot(proportion_data, aes(x = orig.ident, y = proportion, fill = cell_type)) +
   labs(x = "", y = "Proportion", fill = "Cell Type") +  # 设置坐标轴和图例标题
   theme_classic() +  # 使用经典主题（独立坐标轴）
   theme(
-    axis.text.x = element_text(size = 14, angle = 90, hjust = 1, vjust = 0.5),  # 调整横轴字体大小、角度和对齐方式
+    axis.text.x = element_text(size = 18, angle = 90, hjust = 1, vjust = 0.5),  # 调整横轴字体大小、角度和对齐方式
     axis.line = element_line(color = "black"),  # 设置坐标轴颜色
     panel.grid.major = element_blank(),  # 移除主要网格线
-    panel.grid.minor = element_blank()   # 移除次要网格线
+    panel.grid.minor = element_blank(),   # 移除次要网格线
+    legend.text = element_text(size = 28), # 图例文本字体大小
+    legend.title = element_text(size = 28) # 图例标题字体大小（如果有标题的话，这里原代码设置为NULL）
   )
 
 dev.off()
@@ -264,28 +298,31 @@ proportion_data <- merged_seurat_obj@meta.data %>%
     group_by(sample_type, cell_type) %>% summarise(count = n()) %>% mutate(proportion = count / sum(count))
 
 npg_extended <- colorRampPalette(npg_pal)(13)
-png("sampletype_prop1.png", width = 3000, height = 3000, res = 300)  # 设置高分辨率和尺寸
+png("sampletype_prop1.png", width = 6000, height = 3000, res = 300)  # 设置高分辨率和尺寸
 ggplot(proportion_data, aes(x = "", y = proportion, fill = cell_type)) +
-    geom_bar(stat = "identity", width = 1) +         # 堆叠柱状图
-    coord_polar(theta = "y") +                       # 转换为饼图
-    scale_fill_manual(values = npg_extended) +
-    theme_void() +                                   # 空白背景
-    theme(
-        legend.position = "right",                     # 图例放在右侧
-        plot.title = element_blank(),                  # 移除标题
-        # 分面标签设置
-        strip.placement = "outside",                   # 标签放在绘图区域外
-        strip.text = element_text(                     # 标签样式
-            size = 12,                                   # 字体大小
-            face = "bold",                               # 加粗
-            margin = margin(b = 10)                      # 下方留白（避免与饼图重叠）
-        )
-    ) +
-    facet_wrap(
-        ~ sample_type,
-        ncol = 2,
-        strip.position = "bottom"                      # 标签放在下方
-    )
+  geom_bar(stat = "identity", width = 1) +         # 堆叠柱状图
+  coord_polar(theta = "y") +                       # 转换为饼图
+  scale_fill_manual(values = npg_extended) +       # 使用自定义颜色
+  theme_void() +                                   # 空白背景
+  labs(fill = "Cell Type") +
+  theme(
+    legend.position = "right",                     # 图例放在右侧
+    plot.title = element_blank(),                  # 移除标题
+    # 分面标签设置
+    strip.placement = "outside",                   # 标签放在绘图区域外
+    strip.text = element_text(                     # 标签样式
+      size = 40,                                   # 字体大小
+      face = "bold",                               # 加粗
+      margin = margin(b = 10)                      # 下方留白（避免与饼图重叠）
+    ),
+    legend.text = element_text(size = 36),         # 图例文本字体大小
+    legend.title = element_text(size = 36)                 # 移除图例标题
+  ) +
+  facet_wrap(
+    ~ sample_type,
+    ncol = 2,
+    strip.position = "bottom"                      # 标签放在下方
+  )
 dev.off()
 
 png("sampletype_prop2.png", width = 3000, height = 3000, res = 300)  # 设置高分辨率和尺寸
@@ -295,10 +332,16 @@ ggplot(proportion_data, aes(x = sample_type, y = proportion, fill = cell_type)) 
     labs(x = "Sample Type", y = "Proportion", fill = "Cell Type") +  # 设置坐标轴和图例标题
     theme_classic() +  # 使用经典主题（独立坐标轴）
     theme(
-        axis.text.x = element_text(angle = 45, hjust = 1, vjust = 0.5),  # 调整横轴标签角度和对齐方式
+        axis.text.x = element_text(hjust = 0.5, vjust = 0.5, size = 28),  # 调整横轴标签角度和对齐方式
         axis.line = element_line(color = "black"),  # 设置坐标轴颜色
         panel.grid.major = element_blank(),  # 移除主要网格线
-        panel.grid.minor = element_blank()   # 移除次要网格线
+        panel.grid.minor = element_blank(),   # 移除次要网格线
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size = 40),
+
+        axis.text.y = element_text(size = 28),
+        legend.text = element_text(size = 36),         # 图例文本字体大小
+        legend.title = element_text(size = 40)             
     )
 dev.off()
 
@@ -360,21 +403,53 @@ ggplot(cell_proportion, aes(x = cell_type, y = proportion, fill = sample_type)) 
     aes(group = sample_type),  # 按 sample_type 分组比较
     method = "wilcox.test",    # 使用 Wilcoxon 检验
     label = "p.signif",        # 显示显著性符号（如 *）
-    label.y = 1.1,             # 调整标签的垂直位置
-    size = 4                   # 标签字体大小
+    label.y = 0.6,             # 调整标签的垂直位置
+    size = 12                   # 标签字体大小
   ) +
   labs(x = "", y = "Proportion", fill = "Sample Type") +  # 设置坐标轴和图例标题
   theme_classic() +  # 使用经典主题
   theme(
-    axis.text.x = element_text(angle = 45, hjust = 1, size = 14),  # 调整横轴标签角度
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 28),  # 调整横轴标签角度
+    axis.text.y = element_text(size = 28),
+    axis.title.y = element_text(size = 40),
     axis.line = element_line(color = "black"),  # 设置坐标轴颜色
     panel.grid.major = element_blank(),  # 移除主要网格线
     panel.grid.minor = element_blank(),  # 移除次要网格线
+    legend.text = element_text(size = 36),         # 图例文本字体大小
+    legend.title = element_text(size = 40),
     legend.position = "right"  # 设置图例位置在右侧
   ) +
-  scale_fill_npg()
+  scale_fill_npg() +
+  scale_y_continuous(limits = c(0, 0.6)) 
 dev.off()
 
+
+png("count_箱图.png", width = 6000, height = 3000, res = 300)
+ggplot(cell_proportion, aes(x = cell_type, y = count, fill = sample_type)) +
+    geom_boxplot() +  # 绘制箱线图
+    stat_compare_means(  # 添加显著性标记
+        aes(group = sample_type),  # 按 sample_type 分组比较
+        method = "wilcox.test",    # 使用 Wilcoxon 检验
+        label = "p.signif",        # 显示显著性符号（如 *）
+    # 调整标签的垂直位置
+        size = 12                   # 标签字体大小
+    ) +
+    labs(x = "", y = "Count", fill = "Sample Type") +  # 设置坐标轴和图例标题
+    theme_classic() +  # 使用经典主题
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 28),  # 调整横轴标签角度
+        axis.text.y = element_text(size = 28),
+        axis.title.y = element_text(size = 40),
+        axis.line = element_line(color = "black"),  # 设置坐标轴颜色
+        panel.grid.major = element_blank(),  # 移除主要网格线
+        panel.grid.minor = element_blank(),  # 移除次要网格线
+        legend.text = element_text(size = 36),         # 图例文本字体大小
+        legend.title = element_text(size = 40),
+        legend.position = "right"  # 设置图例位置在右侧
+    ) +
+    scale_fill_npg() +
+    scale_y_continuous(limits = c(0, 4000)) 
+dev.off()
 
 identity_mapping <- c(
   "Sample1" = "Ⅲb",
@@ -452,6 +527,7 @@ ggplot(proportion_data, aes(x = "", y = proportion, fill = cell_type)) +
     geom_bar(stat = "identity", width = 1) +         # 堆叠柱状图
     coord_polar(theta = "y") +                       # 转换为饼图
     scale_fill_manual(values = npg_extended) +
+    labs(fill = "Cell Type") +
     theme_void() +                                   # 空白背景
     theme(
         legend.position = "right",                     # 图例放在右侧
@@ -459,10 +535,12 @@ ggplot(proportion_data, aes(x = "", y = proportion, fill = cell_type)) +
         # 分面标签设置
         strip.placement = "outside",                   # 标签放在绘图区域外
         strip.text = element_text(                     # 标签样式
-            size = 12,                                   # 字体大小
+            size = 40,                                   # 字体大小
             face = "bold",                               # 加粗
             margin = margin(b = 10)                      # 下方留白（避免与饼图重叠）
-        )
+        ),
+        legend.text = element_text(size = 36),         # 图例文本字体大小
+        legend.title = element_text(size = 36)  
     ) +
     facet_wrap(
         ~ period,
@@ -470,3 +548,99 @@ ggplot(proportion_data, aes(x = "", y = proportion, fill = cell_type)) +
         strip.position = "bottom"                      # 标签放在下方
     )
 dev.off()
+
+png("period_prop2.png", width = 6000, height = 3000, res = 300)
+cell_counts <- merged_seurat_obj@meta.data %>%
+  group_by(orig.ident, cell_type, period) %>%
+  summarise(count = n()) %>%
+  ungroup()
+
+ggplot(cell_counts, aes(x = cell_type, y = count, fill = period)) +
+  geom_bar(stat = "identity", position = "stack") +  # 堆叠柱状图
+  labs(x = "Cell Type", y = "Count", fill = "Stage") +  # 设置坐标轴和图例标题
+  scale_fill_npg() +  # 使用ggsci的npg配色
+  theme_classic() +  # 使用经典主题
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 28),  # 调整横轴标签角度
+    axis.text.y = element_text(size = 28),
+    axis.title.x = element_blank(),  # 横坐标标题字体大小
+    axis.title.y = element_text(size = 40),  # 纵坐标标题字体大小
+    legend.text = element_text(size = 36),  # 图例文本字体大小
+    legend.title = element_text(size = 40),  # 图例标题字体大小
+    legend.position = "right"  # 将图例放在顶部
+  )
+dev.off()
+
+tumor <- subset(merged_seurat_obj, subset = sample_type == "tumor")
+png("t_period_prop2.png", width = 6000, height = 3000, res = 300)
+cell_counts <- tumor@meta.data %>%
+  group_by(orig.ident, cell_type, period) %>%
+  summarise(count = n()) %>%
+  ungroup()
+
+ggplot(cell_counts, aes(x = cell_type, y = count, fill = period)) +
+  geom_bar(stat = "identity", position = "stack") +  # 堆叠柱状图
+  labs(x = "Cell Type", y = "Count", fill = "Stage") +  # 设置坐标轴和图例标题
+  scale_fill_npg() +  # 使用ggsci的npg配色
+  theme_classic() +  # 使用经典主题
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 28),  # 调整横轴标签角度
+    axis.text.y = element_text(size = 28),
+    axis.title.x = element_blank(),  # 横坐标标题字体大小
+    axis.title.y = element_text(size = 40),  # 纵坐标标题字体大小
+    legend.text = element_text(size = 36),  # 图例文本字体大小
+    legend.title = element_text(size = 40),  # 图例标题字体大小
+    legend.position = "right"  # 将图例放在顶部
+  )
+dev.off()
+
+fibro_data <- subset(cell_proportion, cell_type == "Fibroblast")
+
+#DEGs
+
+genes_to_plot <- c(
+    # 淋巴细胞系
+    "CD3D", "CD3E", "NKG7",          # T细胞
+    "BANK1", "CD79A", "MS4A1",        # B细胞
+    "GNLY", "KLRC1", "KLRB1",       # NK细胞
+    "IGHG1", "IGHG3", "JCHAIN",      # 浆细胞
+    
+    # 髓系免疫细胞
+    "C1QA", "C1QB", "APOC1",         # 巨噬细胞
+    "FCN1", "CD300E", "SLC11A1",          # 单核细胞
+    "CD86", "PKIB", "CLEC10A",      # 树突细胞（修正HAL-DRA为HLA-DRA）
+    
+    # 结构细胞
+    "SFN", "KRT19", "KRT17",       # 上皮细胞
+    "SFRP2", "COL1A2", "FGF7",     # 成纤维细胞
+    "CLDN5", "PECAM1", "RAMP2",      # 内皮细胞
+    "RGS5", "MCAM", "ACTA2",         # 周细胞
+    
+    # 特殊功能细胞
+    "CPA3", "TPSAB1", "TPSB2",       # 肥大细胞
+    "TOP2A", "STMN1", "MKI67"         # 增殖细胞
+)
+png("dotplot.png", width = 8000, height = 3000, res = 300)  # 设置高分辨率和尺寸
+
+DotPlot(merged_seurat_obj, 
+        features = genes_to_plot, 
+        group.by = "cell_type",
+        dot.scale = 15) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 28),  # 横坐标基因名字体大小
+    axis.text.y = element_text(size = 28),  # 纵坐标细胞类型字体大小
+    axis.title.x = element_blank(),  # 去除横坐标标题
+    axis.title.y = element_text(size = 40),
+    panel.grid.major = element_line(color = "grey70", linewidth = 0.5),  # 添加网格线
+    panel.grid.minor = element_line(color = "grey80", linewidth = 0.3),  # 细网格线 
+    legend.text = element_text(size = 28, face = "bold", color = "black"),
+    legend.title = element_text(size = 28, face = "bold", color = "black"),
+    legend.position = "right",
+    legend.box.margin = margin(0, 0, 0, 0),
+    legend.key = element_blank(),
+    legend.background = element_blank()
+  ) +
+  scale_color_gsea()  # 使用ggsci的GSEA配色
+dev.off()
+
+FeaturePlot(merged_seurat_obj, features = genes_to_plot)
--- ESCC/single-cell transcrptome analysis/t_cluster.r
+++ ESCC/single-cell transcrptome analysis/t_cluster.r
@@ -4,26 +4,61 @@ t_cells <- FindVariableFeatures(t_cells, nfeatures = 2000)
 hvgs <- VariableFeatures(t_cells)
 t_cells <- ScaleData(t_cells, features = hvgs)
 t_cells <- RunPCA(t_cells, features = hvgs, npcs = 20)
-t_cells <- RunHarmony(t_cells, "sample_sources")
+t_cells <- RunHarmony(t_cells, "orig.ident")
 t_cells <- RunUMAP(t_cells, dims = 1:15, reduction = "harmony")
 t_cells <- FindNeighbors(t_cells, dims = 1:15, reduction = "harmony")
 t_cells <- FindClusters(t_cells, resolution = 0.3)
 
-DimPlot(t_cells, reduction = "umap", label = TRUE) +
-  xlab("UMAP_1") +
-  ylab("UMAP_2") +
-  ggtitle(NULL) +
-  guides(color = guide_legend(title = NULL, override.aes = list(size = 5))) +
-  theme(
-    text = element_text(size = 8, face = "bold"),
-    axis.text.x = element_text(size = 12), 
-    axis.text.y = element_text(size = 12), 
-    axis.title.x = element_text(size = 12), 
-    axis.title.y = element_text(size = 12), 
-    plot.title = element_text(size = 12),
-    legend.text = element_text(size = 8), # 图例文本字体大小
-    legend.title = element_text(size = 8) # 图例标题字体大小（如果有标题的话，这里原代码设置为NULL）
-  )
+
+
+
+# 获取图例的个数和名称长度
+seurat_clusters <- as.character(unique(t_cells@meta.data$seurat_clusters))  # 转换为字符向量
+num_legend_items <- length(seurat_clusters)  # 图例的个数
+max_label_length <- max(nchar(seurat_clusters))  # 图例名称的最大长度
+
+# 动态计算图片尺寸
+base_width <- 3000  # 基础宽度
+base_height <- 3000  # 基础高度
+legend_width_factor <- 100  # 每个图例项增加的宽度
+label_length_factor <- 10  # 每个字符增加的宽度
+
+# 计算动态宽度
+dynamic_width <- base_width + (num_legend_items * legend_width_factor) + (max_label_length * label_length_factor)
+
+png("t_clusters.png", width = dynamic_width, height = base_height, res = 300)
+DimPlot(t_cells, reduction = "umap", label = TRUE, pt.size = 2, label.size = 8) +
+    xlab("UMAP_1") +
+    ylab("UMAP_2") +
+    ggtitle(NULL) +
+    scale_color_npg() +
+    coord_fixed(ratio = 1) +
+    guides(color = guide_legend(title = NULL, override.aes = list(size = 5))) +
+    theme(
+        text = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 28, color = "black"),
+        axis.text.y = element_text(size = 28, color = "black"),
+        axis.title.x = element_text(size = 36, face = "bold", color = "black"),
+        axis.title.y = element_text(size = 36, face = "bold", color = "black", margin = margin(r = 20)),  # 增加右侧间距
+        plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        legend.text = element_text(size = 28, face = "bold", color = "black"),
+        legend.title = element_text(size = 28, face = "bold", color = "black"),
+        legend.position = "right",
+        legend.box.margin = margin(0, 0, 0, 0),
+        legend.key = element_blank(),
+        legend.background = element_blank(),
+        panel.background = element_rect(fill = "white", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line.x = element_line(color = "black", linewidth = 0.5),
+        axis.line.y = element_line(color = "black", linewidth = 0.5),
+        aspect.ratio = 1,
+        plot.margin = margin(10, 50, 10, 10)
+    )
+dev.off()
+
+
 
 #annotation
 t_cell_markers <- FindAllMarkers(t_cells, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25, test.use = "wilcox")
@@ -32,41 +67,80 @@ write.csv(t_significant_markers, "t_all_marker.csv")
 t_significant_markers <- t_significant_markers %>% group_by(cluster) %>% top_n(n = 50, wt = avg_log2FC)
 write.csv(t_significant_markers, "t_top_marker.csv")
 
-b_cluster_ids <- c(6, 9, 10)
+b_cluster_ids <- c(6, 8, 9)
 t_cells <- subset(t_cells, idents = setdiff(levels(Idents(t_cells)), b_cluster_ids))
 
+png("t_fature.png", width = dynamic_width, height = base_height, res = 300)
+FeaturePlot(t_cells, features = c("CD4", "CD8A", "KLRB1", "FOXP3", 
+                  "CRTAM", "IFNG", "FOSB","ITGA1", 
+                  "KLRF1", "HAVCR2", "CXCL13")) 
+dev.off()
+
 identity_mapping <- c(
-  "0" = "Tn",
-  "1" = "CD8+Tem",
-  "2" = "Treg", 
-  "3" = "CD8+Teff",
-  "4" = "CD8+Teff",
-  "5" = "CD8+Tex",
+  "0" = "KLRB1+ MAIT",
+  "1" = "FOXP3+ Treg",
+  "2" = "CRTAM+ CD8+Trm", 
+  "3" = "IFNG+ CD8+Teff",
+  "4" = "Early Activated T",
+  "5" = "ITGA1+ CD8+Trm",
   "6" = "TNK",
-  "7" = "TNK",
-  "8" = "CD4+Tex",
-  "9" = "Th17"
+  "7" = "HAVCR2+ CD8+Tex",
+  "8" = "CXCL13+ Tfh"
 )
 cell_type <- identity_mapping[t_cells@meta.data$seurat_clusters]
 t_cells@meta.data$cell_type <- cell_type
 
-DimPlot(t_cells, reduction = "umap", label = TRUE, group.by = "cell_type") +
-  xlab("UMAP_1") +
-  ylab("UMAP_2") +
-  ggtitle(NULL) +
-  guides(color = guide_legend(title = NULL, override.aes = list(size = 5))) +
-  theme(
-    text = element_text(size = 8, face = "bold"),
-    axis.text.x = element_text(size = 12), 
-    axis.text.y = element_text(size = 12), 
-    axis.title.x = element_text(size = 12), 
-    axis.title.y = element_text(size = 12), 
-    plot.title = element_text(size = 12),
-    legend.text = element_text(size = 8), # 图例文本字体大小
-    legend.title = element_text(size = 8) # 图例标题字体大小（如果有标题的话，这里原代码设置为NULL）
-  )
+# 将 cell_type 列转换为因子，并指定顺序
+t_cells@meta.data$cell_type <- factor(t_cells@meta.data$cell_type, levels = identity_mapping)
 
-CD8 <- subset(t_cells, subset = seurat_clusters %in% c(1, 3, 4, 5))
+# 获取唯一的细胞类型并转换为字符向量
+cell_types <- as.character(unique(t_cells@meta.data$cell_type))
+num_legend_items <- length(cell_types)  # 图例的个数
+max_label_length <- max(nchar(cell_types))  # 图例名称的最大长度
+
+# 动态计算图片尺寸
+base_width <- 3000  # 基础宽度
+base_height <- 3000  # 基础高度
+legend_width_factor <- 100  # 每个图例项增加的宽度
+label_length_factor <- 10  # 每个字符增加的宽度
+
+# 计算动态宽度
+dynamic_width <- base_width + (num_legend_items * legend_width_factor) + (max_label_length * label_length_factor)
+
+# 保存图片
+png("t_annotation.png", width = dynamic_width, height = base_height, res = 300)
+DimPlot(t_cells, reduction = "umap", label = TRUE, pt.size = 2, label.size = 8, group.by = "cell_type") +
+    xlab("UMAP_1") +
+    ylab("UMAP_2") +
+    ggtitle(NULL) +
+    scale_color_npg() +
+    coord_fixed(ratio = 1) +
+    guides(color = guide_legend(title = NULL, override.aes = list(size = 5))) +
+    theme(
+        text = element_text(size = 12, face = "bold"),
+        axis.text.x = element_text(size = 28, color = "black"),
+        axis.text.y = element_text(size = 28, color = "black"),
+        axis.title.x = element_text(size = 36, face = "bold", color = "black"),
+        axis.title.y = element_text(size = 36, face = "bold", color = "black", margin = margin(r = 20)),  # 增加右侧间距
+        plot.title = element_text(size = 16, face = "bold", hjust = 0.5),
+        legend.text = element_text(size = 28, face = "bold", color = "black"),
+        legend.title = element_text(size = 28, face = "bold", color = "black"),
+        legend.position = "right",
+        legend.box.margin = margin(0, 0, 0, 0),
+        legend.key = element_blank(),
+        legend.background = element_blank(),
+        panel.background = element_rect(fill = "white", color = NA),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.line.x = element_line(color = "black", linewidth = 0.5),
+        axis.line.y = element_line(color = "black", linewidth = 0.5),
+        aspect.ratio = 1,
+        plot.margin = margin(10, 50, 10, 10)
+    )
+dev.off()
+
+CD8 <- subset(t_cells, subset = seurat_clusters %in% c(2, 3, 5, 7))
 #convert Seurat_obj to SingleCellExperiment_obj
 cd8_sce <- as.SingleCellExperiment(CD8)
 
